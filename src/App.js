33af365d6a9f330d441b5cc26b4a4b89c507cca2
linchengyang <18960807585@163.com>
2021-04-25 22:37:31 +0800

    Introduce Ant components to realize a simple prototype of the head navigation bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {Row, Col} from 'antd';
+import 'antd/dist/antd.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="menu">
+        <Row>
+          <Col span={12}/>
+          <Col span={1}><a href={"#"}>HOME</a></Col>
+          <Col span={1}><a href={"#"}>WHITEPAPER</a></Col>
+          <Col span={1}><a href={"#"}>FEATURES</a></Col>
+          <Col span={1}><a href={"#"}>ORACLE</a></Col>
+          <Col span={1}><a href={"#"}>TAME</a></Col>
+          <Col span={1}><a href={"#"}>CONTACT</a></Col>
+        </Row>
+      </div>
     </div>
   );
 }
